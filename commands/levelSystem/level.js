38e5ab237dd57a39c2954aa6decde0ba12d49edd
Blocky7277 <57508798+Blocky7277@users.noreply.github.com>
2024-08-18 21:45:12 -0500

    View level command works.
    insert a pattern.jpg image into the app root directory and it will work. I need to find a copyright free one to include for a preview. I used canvas to make a custom level card for each user. Then I send that as an attachment in a defered reply.

--- commands/levelSystem/level.js
+++ commands/levelSystem/level.js
@@ -1,4 +1,7 @@
-const { SlashCommandBuilder, SlashCommandUserOption } = require('discord.js');
+const { SlashCommandBuilder, MessageAttachment, AttachmentBuilder } = require('discord.js');
+const {createCanvas, loadImage, registerFont} = require("canvas");
+const {fetchUserData, lvlScale} = require("../../module/levelingSystem.js")
+const sleep = require("../../utility/sleep.js");
 
 module.exports = {
 	data: new SlashCommandBuilder()
@@ -10,6 +13,99 @@ module.exports = {
         .setRequired(true)
     ),
 	async call(interaction) {
-		await interaction.reply('asdf!');
-	},
+        interaction.deferReply();
+        await sleep(250)
+        let target = interaction.options.getUser("target");
+        if (!target) {
+            target = interaction.user;
+        }
+        if(!target?.id) return interaction.editReply({content: "Seems like this user doesn't exist or isn't in this server."});
+        else if(target.bot) return interaction.editReply({content: `Bot XP isn't tracked so ${target.username} does not have an XP profile.`});
+        const user = await fetchUserData(target.id, interaction.guild.id);
+        if (!user) return interaction.editReply({content: `Seems like ${target.globalName} has not earned any xp so far.`}); // If there isnt such user in the database, we send a message
+        registerFont('AreaKilometer50.ttf', {
+            family:'AreaKilometer50',
+        })
+        // var rank;
+        // for (let i = 0; i < rawLeaderboard.length; i++) {
+        //     if (rawLeaderboard[i].userID == user.userID){
+        //         rank = i + 1;
+        //     }
+        // }
+        const data = {
+            level: user.level + 1,
+            xp: user.xp,
+            nextlvl: lvlScale[user.level + 1],
+            // rank,
+        };
+        const canvas = createCanvas(500, 150),
+        cWidth = canvas.width,
+        cHeight = canvas.height;
+        const ctx = canvas.getContext('2d'),
+        bar_width = 300;
+        const avatar = await loadImage(target.avatarURL({format:'png', dynamic:false}).replace('webp','png'));
+        const bg = await loadImage("pattern.jpg");
+        //Background Image
+        ctx.drawImage(bg, 0, 0, cWidth, cHeight);
+    
+    
+        //Begin Drawing
+        ctx.beginPath();
+        ctx.arc(65, 80, 55, 0, 2 * Math.PI);
+        ctx.lineWidth = 2;
+        ctx.strokeStyle = 'white';
+        ctx.stroke();
+        ctx.closePath();
+
+        //XP Bar
+        ctx.lineJoin = 'round';
+        ctx.lineWidth = 35
+        
+        //shadow
+        ctx.strokeStyle = '#C0C0C0'
+        ctx.strokeRect(150, 126, bar_width, 0)
+        
+        //Empty
+        ctx.strokeStyle = 'black'
+        ctx.strokeRect(150, 125, bar_width, 0)
+
+        //Filled Bar
+        ctx.strokeStyle = '#333b66'
+        ctx.strokeRect(150, 125, bar_width * data.xp / data.nextlvl, 0)
+
+        //Text 
+        ctx.font = '40px AreaKilometer50';
+        ctx.fillStyle = 'white';
+        ctx.textAlign = 'center';
+        
+        ctx.fillText(`RANK`, 250, 40, 75)
+        ctx.fillText(`LEVEL`, 405, 40, 75)
+        
+        ctx.font = '40px Arial';
+        // ctx.fillText(`#${data.rank}`, 320, 40, 50)
+        ctx.fillText(`${data.level}`, 465, 40, 50)
+        
+        ctx.font = '20px AreaKilometer50';
+        ctx.textAlign = 'left';
+        ctx.fillText(`${target.globalName}`, 147, 105, 200)
+        
+        ctx.textAlign = 'left';
+        ctx.fillText(`${data.xp}/${data.nextlvl} XP`, 355, 105, 100)
+        
+        //Remove corners
+        ctx.beginPath();
+        ctx.arc(65, 80, 55, 0, 2 * Math.PI);
+        ctx.closePath();
+        ctx.clip()
+
+        //Draw Avatar
+        ctx.drawImage(avatar, 10    , 25, 120, 110);
+        
+        //Send the rank card
+        const at =  new AttachmentBuilder().setFile(canvas.toBuffer(), "rank.png")
+        await interaction.editReply({
+            files: [at]
+        })
+        // await interaction.editReply('Pong!');
+    },
 };
--- module/levelingSystem.js
+++ module/levelingSystem.js
@@ -1,9 +1,9 @@
 const userProfiles = require("../schemas/player.js");
 const mongoose = require("mongoose");
 
-var lvlScale = [0,100,200,300,400,500,600,700,800,900,1000,1200,1400,1600,1800,2000,2250,2500,2750,3000,3500,4000,5000,6000,7000,8000,9000,10000,20000,30000,40000,50000];
 
 class lvlSys {
+    static lvlScale = [0,100,200,300,400,500,600,700,800,900,1000,1200,1400,1600,1800,2000,2250,2500,2750,3000,3500,4000,5000,6000,7000,8000,9000,10000,20000,30000,40000,50000];
     static async createUser(userId, guildId) {
         if (!userId) return console.error("No User ID found")
         if (!guildId) return console.error("No Guild ID found")
@@ -25,7 +25,7 @@ class lvlSys {
         let leveledUp = false;
         if (!userExists) return;
         userExists.xp += xp;
-        if (userExists.xp >= lvlScale[userExists.level]) {
+        if (userExists.xp >= lvlSys.lvlScale[userExists.level]) {
             userExists.level++;
             leveledUp = true;
         }
@@ -34,6 +34,24 @@ class lvlSys {
             user: userExists,
             leveledUp
         };
+    }
+
+    static async fetchUserData(userId, guildId) {
+        if (!userId) {
+            console.error("No User ID found");
+            return null; 
+        }
+        if (!guildId) {
+            console.error("No Guild ID found");
+            return null;
+        }
+        const userExists = await userProfiles.findOne({ userId: userId, guildId: guildId });
+        if (!userExists) 
+            {
+                console.error("No person found");
+                return null;
+            }
+        return userExists;
     }
 
     static async toggleNotifications(userId, notifications) {
